20e78fe0d08ccaba2a18d42db11f72792b254150
Davide Taviani <helios89@gmail.com>
2020-03-18 19:51:07 +0100

    try rewrite using tailwind

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+const tailwindcss = require('tailwindcss');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+const plugins = [];
+plugins.push(tailwindcss)
+// plugins.push(tailwindcss('tailwind.config.js'))
+// This is if you want to include your custom config
+
+if (!IS_DEVELOPMENT) {
+    const purgecss = require('@fullhuman/postcss-purgecss');
+
+    class TailwindExtractor {
+        static extract(content) {
+            return content.match(/[A-z0-9-:\/]+/g) || [];
+        }
+    }
+
+    plugins.push(
+        purgecss({
+            content: ['src/*.html'],
+            extractors: [
+                {
+                    extractor: TailwindExtractor,
+                    extensions: ['html']
+                }
+            ],
+        })
+    );
+}
+
+module.exports = { plugins }
