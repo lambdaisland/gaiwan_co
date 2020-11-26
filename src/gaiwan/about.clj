c2729a32f2b4283ecb7a64154469a73f54f7dd08
Mitesh Shah <mitesh@miteshshah.com>
2020-11-26 12:03:08 +0530

    feat: update team description

--- src/gaiwan/about.clj
+++ src/gaiwan/about.clj
@@ -10,7 +10,7 @@
    [:p "Far from the valleys of silicon we’re a small remote-first and highly distributed company with 5 members situated in 5 different countries. Experts at Clojure, Clojurescript, Datomic, and Emacs &mdash; we have built and launched multiple products around these technologies."]
    [:p "Simplicity, and honesty are our core values. Our globally diverse team gives us a huge advantage, as every single member brings a different perspective to the table."]])
 
-(def member-data 
+(def member-data
   [{:title "Arne Brasseur - Berlin"
     :subtitle "(@plexus)"
     :description '([:p "Arne has been programming for over a decade in several different industries and programming languages."]
@@ -19,11 +19,23 @@
                    [:p "He loves drinking tea, and taking long runs in the nature!"])}
    {:title "Felipe Barros - Recife"
     :subtitle "(@barrosfelipe)"
-    :description '([:p "Felipe has been using Linux since as long as he can remember. Being an Emacs wizard with an eye for design gives him superpowers!"]
-                   [:p "He loves using Clojure and Clojurescript."])}
+    :description '([:p "While still at school, GNU/Linux liberated Felipe from
+                   the shackles of digital oblivion and corporate abuse."]
+                   [:p "Being a keyboard enthusiast who follows the Dvorak
+                   branch of the Emacs faith, he already manipulated text in Vim
+                   and Emacs way before his renewed interest in computer
+                   programming and cherry-picked the Clojure/ClojureScript path
+                   after falling in love with Rich Hickey’s capillary
+                   parentheses/curls."]
+                   [:p "An arts person, likes to spend free time writing or
+                   drawing and can often be caught talking about interfaces as
+                   handcrafted, artisanal work."])}
    {:title "Ariel Alexi - Rosh HaAyin"
     :subtitle "(@ArielA147)"
-    :description '([:p "Ariel joined Gaiwan immediately after finishing her univerity in Computer science. Having built numerous projects with C++, Java, Javascript, and Go, her thirst for knowledge has not stopped growing. She is now progressing her way through Clojure."]
+    :description '([:p "Ariel is currently a 4th year Computer Science student.
+                    Having built numerous projects with C++, Java, Javascript,
+                    and Go, her thirst for knowledge has not stopped growing.
+                    She is now progressing her way through Clojure."]
                    [:p "In her free time she likes to develop video games."])}
    {:title "Alys Brooks - Madison"
     :subtitle "(@alyssbrooks)"
@@ -32,8 +44,8 @@
    {:title "Mitesh Shah - Mumbai"
     :subtitle "(@oxalorg)"
     :description '([:p "Mitesh learnt coding because of his desire to hack video games. This slowly turned to a passion for all things computer."]
-                  [:p "Since then he's launched multiple online products, co-founded his own e-grocery vegan startup, and consulted on various technologies and projects."]
-                  [:p "He loves video games, dabbles in moral philosophy, and is trying to go vegan."])}])
+                   [:p "Since then he's launched multiple online products, co-founded his own e-grocery vegan startup, and consulted on various technologies and projects."]
+                   [:p "He loves video games, dabbles in moral philosophy, and is trying to go vegan."])}])
 
 (defn- team-member [data]
   (let [{:keys [title subtitle description]} data]
@@ -44,7 +56,7 @@
 
 (def team-members
   [:div
-   (for [member member-data] 
+   (for [member member-data]
      (team-member member))])
 
 (def team
@@ -58,7 +70,7 @@
     [:a {:href "mailto:arne@gaiwan.co"} "Come work with us!"]]])
 
 (def body
-  [:div.about-container 
+  [:div.about-container
    navbar summary team work-with-us])
 
 (defn about-page [content]
