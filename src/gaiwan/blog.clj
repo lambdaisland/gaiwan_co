f59f8a99fe0bdfe58aafd54864209b4718c4714f
Mitesh Shah <mitesh@miteshshah.com>
2021-01-22 11:32:52 +0530

    feat: blog foundation

--- src/gaiwan/blog.clj
+++ src/gaiwan/blog.clj
@@ -1,10 +1,23 @@
 (ns gaiwan.blog
-  (:require [gaiwan.common :as common]))
+  (:require [gaiwan.common :as common]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn get-posts []
+  (filter
+   #(str/ends-with? (.getName %) ".md")
+   (file-seq (io/file (io/resource "posts")))))
+
+(def post-list
+  [:ul
+   (for [post (get-posts)]
+     [:li (.getName post)])])
 
 (def body
   [:body
    [:main
-    [:h2 "Blog posts"]]
+    [:h2 "Blog posts"]
+    post-list]
    common/footer])
 
 (defn blog-page [content]
