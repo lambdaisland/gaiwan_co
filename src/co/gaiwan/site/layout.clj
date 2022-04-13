2b9b61bf676836f5e971c23be4fe6c3e601709c5
Arne Brasseur <arne@arnebrasseur.net>
2022-04-13 09:08:45 +0200

    Add favicon

--- src/co/gaiwan/site/layout.clj
+++ src/co/gaiwan/site/layout.clj
@@ -45,6 +45,10 @@
      [:link {:href "/assets/css/sakura-ink-embed.css", :rel "stylesheet"}]
      [:link {:href "/assets/css/style.css", :rel "stylesheet"}]
      [:link {:rel "icon" :href "/favicon.ico" :type "image/png"}]
+     [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/apple-touch-icon.png"}]
+     [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/favicon-32x32.png"}]
+     [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/favicon-16x16.png"}]
+     [:link {:rel "manifest" :href "/site.webmanifest"}]
      matamo-analytics-script
      [:link {:href "https://use.typekit.net/oxk2nmu.css", :rel "stylesheet"}]
      #_(if (io/resource "assets/css/ornament.css")
